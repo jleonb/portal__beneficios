e195dd75a67aacc369fcb28564a169f5f6d2fb13
jleon <jleon@webframe.cl>
2016-05-03 15:47:11 -0300

    .

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 Rails.application.routes.draw do
   devise_for :admins
-  devise_for :users
   resources :agreements
   resources :tops
   resources :nexus
   resources :users
 
   root 'agreements#index'
-
-  devise_scope :users do
-    get '/edit', :to => 'users#edit', as: :login
-  end
 
 end
